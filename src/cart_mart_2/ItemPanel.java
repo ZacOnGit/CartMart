b411b303c2820d23d24faf465b4fe985b43c12c7
David <ds0130@uah.edu>
2022-11-28 10:11:25 -0600

    done

--- src/cart_mart_2/ItemPanel.java
+++ src/cart_mart_2/ItemPanel.java
@@ -17,11 +17,13 @@ import javax.swing.SwingUtilities;
  *
  * @author dcsv4
  */
-public class ItemPanel extends JPanel{
+public class ItemPanel extends JPanel {
+
     int itemQuantity;
     public static int cartCount = 0;
     private static Order order;
-    public ItemPanel(int itemNum, String itemName, String itemDesc, int itemPrice, int itemQty, int orderQty){
+
+    public ItemPanel(int itemNum, String itemName, String itemDesc, int itemPrice, int itemQty, int orderQty) {
         JLabel image = new javax.swing.JLabel();
         JLabel name = new javax.swing.JLabel();
         JLabel description = new javax.swing.JLabel();
@@ -34,13 +36,13 @@ public class ItemPanel extends JPanel{
         lineTotal.setVisible(false);
         this.setBackground(Color.WHITE);
         itemQuantity = itemQty;
-        if (order == null)
+        if (order == null) {
             order = new Order();
-        try{
-            ImageIcon pic = new ImageIcon(Cart_Mart_2.class.getResource("images/"+String.valueOf(itemNum)+".jpg"));
-            image.setIcon(pic);
         }
-        catch (Exception e){
+        try {
+            ImageIcon pic = new ImageIcon(Cart_Mart_2.class.getResource("images/" + String.valueOf(itemNum) + ".jpg"));
+            image.setIcon(pic);
+        } catch (Exception e) {
             ImageIcon pic = new ImageIcon(Cart_Mart_2.class.getResource("images/missing.jpg"));
             image.setIcon(pic);
         }
@@ -48,24 +50,24 @@ public class ItemPanel extends JPanel{
         image.setMaximumSize(new java.awt.Dimension(100, 100));
         image.setMinimumSize(new java.awt.Dimension(100, 100));
         image.setPreferredSize(new java.awt.Dimension(100, 100));
-        
+
         name.setText(itemName);
         name.setMaximumSize(new java.awt.Dimension(160, 20));
         name.setMinimumSize(new java.awt.Dimension(160, 20));
         name.setPreferredSize(new java.awt.Dimension(160, 20));
         name.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 18));
-        
+
         description.setText(itemDesc);
         description.setMaximumSize(new java.awt.Dimension(160, 20));
         description.setMinimumSize(new java.awt.Dimension(160, 20));
         description.setPreferredSize(new java.awt.Dimension(160, 20));
         description.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 18));
-        price.setText("$"+String.format("%.2f", (float)itemPrice/100.0));
+        price.setText("$" + String.format("%.2f", (float) itemPrice / 100.0));
         quantity.setText(String.valueOf(orderQty));
         incButton.setText("+");
         decButton.setText("-");
         addToCart.setText("ADD");
-        
+
         incButton.setPreferredSize(new java.awt.Dimension(41, 30));
         decButton.setPreferredSize(new java.awt.Dimension(41, 30));
         addToCart.setPreferredSize(new java.awt.Dimension(60, 30));
@@ -74,17 +76,17 @@ public class ItemPanel extends JPanel{
         quantity.setHorizontalAlignment(JTextField.CENTER);
         quantity.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 18));
         price.setFont(new java.awt.Font("Franklin Gothic Demi", 0, 18));
-        
-        if (Cart_Mart_2.inCart){
+
+        if (Cart_Mart_2.inCart) {
             addToCart.setPreferredSize(new java.awt.Dimension(80, 30));
             addToCart.setText("Update");
         }
-        if (Cart_Mart_2.inReceipt){
+        if (Cart_Mart_2.inReceipt) {
             incButton.setVisible(false);
             decButton.setVisible(false);
             addToCart.setVisible(false);
             lineTotal.setText(String.valueOf(25));
-            
+
             lineTotal.setVisible(true);
         }
         incButton.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -101,89 +103,100 @@ public class ItemPanel extends JPanel{
         });
         addToCart.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
-            public void mouseClicked(java.awt.event.MouseEvent evt) { 
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
                 //System.out.println(itemNum);
-                if (Integer.valueOf(quantity.getText()) != 0){
+                if (Integer.valueOf(quantity.getText()) != 0) {
                     Boolean itemInCart = false;
-                    for (int i = 0; i < order.itemIdList.size(); i++){
-                        if (order.itemIdList.get(i) == itemNum){
-                                itemInCart = true;
-                                order.qtyList.set(i, Integer.valueOf(quantity.getText()));
-                        }                        
+                    for (int i = 0; i < order.itemIdList.size(); i++) {
+                        if (order.itemIdList.get(i) == itemNum) {
+                            itemInCart = true;
+                            order.qtyList.set(i, Integer.valueOf(quantity.getText()));
+                        }
                     }
-                    if (!itemInCart){
+                    if (!itemInCart) {
                         cartCount++;
                         order.itemIdList.add(itemNum);
                         order.qtyList.add(Integer.valueOf(quantity.getText()));
-                    }  
-                }            
-                if (!Cart_Mart_2.inCart){
+                    }
+                }
+                if (Integer.valueOf(quantity.getText()) == 0 && Cart_Mart_2.inCart) {
+                    for (int i = 0; i < order.itemIdList.size(); i++) {
+                        if (order.itemIdList.get(i) == itemNum) {
+                            if (Integer.valueOf(quantity.getText()) == 0) {
+                                order.itemIdList.remove(i);
+                                order.qtyList.remove(i);
+                                cartCount--;
+                                Cart_Mart_2.refresh(cartCount);
+                            }
+                        }
+                    }
+                }
+                if (!Cart_Mart_2.inCart) {
                     Cart_Mart_2.refresh(cartCount);
-                    //quantity.setText(String.valueOf(0));
                 }
             }
         });
-        
+
         javax.swing.GroupLayout itemPanelLayout = new javax.swing.GroupLayout(this);
         this.setLayout(itemPanelLayout);
         itemPanelLayout.setHorizontalGroup(
-            itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(itemPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(image, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(itemPanelLayout.createSequentialGroup()
-                        .addComponent(name, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(217, 217, 217))
-                    .addGroup(itemPanelLayout.createSequentialGroup()
-                        .addComponent(description, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(38, 38, 38)
-                        .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(33, 33, 33)
-                        .addComponent(incButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(decButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)   
-                        .addComponent(addToCart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(23, Short.MAX_VALUE))))
+                itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(itemPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(image, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(itemPanelLayout.createSequentialGroup()
+                                                .addComponent(name, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addGap(217, 217, 217))
+                                        .addGroup(itemPanelLayout.createSequentialGroup()
+                                                .addComponent(description, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(38, 38, 38)
+                                                .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(33, 33, 33)
+                                                .addComponent(incButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(decButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(addToCart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addContainerGap(23, Short.MAX_VALUE))))
         );
         itemPanelLayout.setVerticalGroup(
-            itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(itemPanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(image, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(itemPanelLayout.createSequentialGroup()
-                        .addComponent(name, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(description, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(incButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(decButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(addToCart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        ); 
-        
-        
+                itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(itemPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(image, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(itemPanelLayout.createSequentialGroup()
+                                                .addComponent(name, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addGroup(itemPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                        .addComponent(description, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(price, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(incButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(quantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(decButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addComponent(addToCart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
     }
-    
-    private int incButtonMouseClicked(java.awt.event.MouseEvent evt, int num){
-        if (num == itemQuantity){
-            JOptionPane.showMessageDialog(null, String.valueOf(num)+" is the maximum allowed.");
+
+    private int incButtonMouseClicked(java.awt.event.MouseEvent evt, int num) {
+        if (num == itemQuantity) {
+            JOptionPane.showMessageDialog(null, String.valueOf(num) + " is the maximum allowed.");
             return num;
-        }            
+        }
         num++;
-        return num;            
+        return num;
     }
-    
-    private int decButtonMouseClicked(java.awt.event.MouseEvent evt, int num){
-        if (num == 0)
+
+    private int decButtonMouseClicked(java.awt.event.MouseEvent evt, int num) {
+        if (num == 0) {
             return num;
+        }
         num--;
         return num;
     }
